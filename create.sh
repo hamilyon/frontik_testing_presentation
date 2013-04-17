4855ea734d14a51cf144e110f68e79a90ef47dd1
A.Shaposhnikov <a.shaposhnikov@hh.ru>
2013-04-17 16:21:02 +0400

    rename presentation file

--- create.sh
+++ create.sh
@@ -10,4 +10,4 @@ grep -v usepackage test.tex  | grep -v document | grep -v documentclass > test2.
 pygmentize -f latex -O full mocks.py > mocks.tex
 grep -v usepackage mocks.tex  | grep -v document | grep -v documentclass mocks.tex
 
-xelatex -shell-escape hh-sites-common-tests.tex
+xelatex -shell-escape logic_tests.tex
